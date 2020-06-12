2d84c627710b4b89a6f5235615b56a820b96b06f
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-06-12 10:29:56 -0400

    Clean up and add prop types

--- src/components/RangeSlider/Axis.js
+++ src/components/RangeSlider/Axis.js
@@ -1,8 +1,7 @@
 import React from "react";
 import * as d3 from "d3";
-import styled from "styled-components";
 import withResponsiveContainer from "./withResponsiveContainer"
-
+import PropTypes from 'prop-types';
 
 class Axis extends React.Component {
   constructor() {
@@ -19,14 +18,14 @@ class Axis extends React.Component {
   }
 
   d3Render() {
-    const { type, scale, width, height, marginLeft, marginRight } = this.props;
+    const { type, scale, width, marginLeft, marginRight } = this.props;
     const adjustedWidth = width - (marginLeft + marginRight);
     scale.range([0,adjustedWidth])
     d3.select(this.gRef.current).call(d3[`axis${type}`](scale).tickValues([2, 2.5, 3, 3.5, 4, 4.5, 5]));
   }
 
   render() {
-    const { marginLeft, label, height } = this.props;
+    const { marginLeft, height } = this.props;
 
     return (
       <g ref={this.gRef} transform={`translate(${marginLeft}, ${height})`} className="range-slider__axis">
@@ -38,4 +37,14 @@ class Axis extends React.Component {
 const ResponsiveAxis = withResponsiveContainer(Axis, { height: 20, margin: { top: 0, bottom: 0, right: 0, left: 0}})
 
 
+Axis.propTypes = {
+  type: PropTypes.string.isRequired,
+  scale: PropTypes.func.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  marginLeft: PropTypes.number.isRequired,
+  marginRight: PropTypes.number.isRequired
+}
+
 export default ResponsiveAxis;
+
